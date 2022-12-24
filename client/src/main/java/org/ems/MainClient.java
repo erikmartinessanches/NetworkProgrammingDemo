d4993bbc14147d8813e7399379040c4052938913
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-12-24 03:32:34 +0100

    Able to build and run (with gradle) the modules 'server' and 'client' as independent items inside the project.

---
+++ client/src/main/java/org/ems/MainClient.java
@@ -0,0 +1,7 @@
+package org.ems;
+
+public class MainClient {
+    public static void main(String[] args) {
+        System.out.println("Hello Client!");
+    }
+}
---
+++ server/src/main/java/org/ems/MainServer.java
@@ -0,0 +1,7 @@
+package org.ems;
+
+public class MainServer {
+    public static void main(String[] args) {
+        System.out.println("Hello Server!");
+    }
+}
